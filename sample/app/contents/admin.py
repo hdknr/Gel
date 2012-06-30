7c873712f3641d318e3f67136d54bc734a0145e9
hdknr <gmail@hdknr.com>
2012-06-30 16:34:22 +0900

    admin for Page

--- sample/app/contents/admin.py
+++ sample/app/contents/admin.py
@@ -7,3 +7,7 @@ class ContentAdmin(admin.ModelAdmin):
     list_display=('slug','title','media','text',)
 admin.site.register(Content,ContentAdmin)
 
+### Page
+class PageAdmin(admin.ModelAdmin):
+    list_display=('title',)
+admin.site.register(Page,PageAdmin)
