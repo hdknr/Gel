04673c9a12a0a38e4cacb3de444333c1785b901f
hdknr <gmail@hdknr.com>
2012-07-03 03:22:21 +0900

    MEDIA_ROOT copied to static site

--- src/gel/management/commands/gel.py
+++ src/gel/management/commands/gel.py
@@ -38,15 +38,27 @@ class Command(BaseCommand):
         open(DEFAULT_DIR + urlpath, 'w').write(r.content)
 
     def handle_default(self, *args, **options):
+        ''' Run under project root
+        '''
+
         #: ROOT
         if not os.path.isdir(options['dir']):
             os.makedirs(options['dir'] ) 
 
         #: STATIC
-        self.STATIC = options['dir'] +"/static" 
+        self.STATIC = os.path.join(options['dir'] ,"static"  )
         if os.path.isdir(self.STATIC):
             shutil.rmtree(self.STATIC) 
         shutil.copytree('static',self.STATIC )       #: TODO "static" should be given as option
+
+        #: MEDIA 
+        #: MEDIA_ROOT shoul be relative to project root ( as of now )
+        if settings.MEDIA_ROOT != "":
+            print settings.MEDIA_ROOT
+            self.MEDIA = os.path.join(options['dir'] , settings.MEDIA_ROOT )
+            if os.path.isdir(self.MEDIA):
+                shutil.rmtree(self.MEDIA) 
+            shutil.copytree(settings.MEDIA_ROOT,self.MEDIA)  
 
         #: URLS
         m = __import__(settings.GEL_MODULE,globals(),locals(), 
